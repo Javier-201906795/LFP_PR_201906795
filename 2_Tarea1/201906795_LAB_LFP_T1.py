3d3c962f33ef613452b70a5a2da5c036a628ff44
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2022-02-10 10:28:22 -0600

    Leer texto del archivo
    se lee el texto del archivo lfp

--- 2_Tarea1/201906795_LAB_LFP_T1.py
+++ 2_Tarea1/201906795_LAB_LFP_T1.py
@@ -6,15 +6,30 @@ def abrirarchivo():
     Tk().withdraw()
     archivo = filedialog.askopenfile(
         title = "Selecciona un archivo",
+        #accede a la carpeta donde esta el archivo py
         initialdir =  "./",
+        #tipo de archivo que puede seleccionar
         filetype = [
             ("Archivos LFP", "*.lfp"),
             ("Todos los archivos", "*.*")
         ]
     )
 
+    #Leer archivo
+    if archivo is None:
+        print("No se selecciono ningun archivo" + "\n")
+        return None
+    else:
+        #Devolver Texto del archivo
+        txt = archivo.read()
+        archivo.close()
+        return txt
 
 
 #Main program
 if __name__ == "__main__":
+    #cargar el archivo lfp con el texto a inspeccionar
     texto = abrirarchivo()
+
+    #test
+    print(texto)
