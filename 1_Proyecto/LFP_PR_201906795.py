d9725123cf6a2c7aa2fc3c28b85fa5ef2ece7aa7
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2022-02-17 10:33:54 -0600

    abrir archivo data
    con codificacion utf8 para detectar la letra ñ y las comillas dobles "

--- 1_Proyecto/LFP_PR_201906795.py
+++ 1_Proyecto/LFP_PR_201906795.py
@@ -2,17 +2,12 @@
 from tkinter import filedialog, Tk
 #Importaciones Funciones
 from FuncionesGenerales import FuncionesGenerales
-from Modulos import Modulos
 
 ################################################################
-#Configuracion librerias explorador de archivos
 def abrirarchivodata():
-    #Mesaje
-    print("Selecciona el archivo .data y cargalo en el programa")
-    Tk().withdraw()
-    archivo = filedialog.askopenfile(
+    archivo = filedialog.askopenfilename(
         title = "Selecciona un archivo",
-        #accede a la carpeta donde esta el archivo py
+        #accede a la carpeta donde esta el archivo 
         initialdir =  "./",
         #tipo de archivo que puede seleccionar
         filetype = [
@@ -21,16 +16,18 @@ def abrirarchivodata():
         ]
     )
 
-    #Leer archivo
+
     if archivo is None:
         print("No se selecciono ningun archivo" + "\n")
         return None
     else:
-        #Devolver Texto del archivo
-        txt = archivo.read()
-        archivo.close()
-        return txt
+        #Abre el archivo
+        with open(archivo, 'r', encoding='utf8') as file:
+            text = file.read()
+            file.close()
+        return text
 
+    
 ################################################################
 ################################################################
 #Main program
@@ -38,16 +35,15 @@ if __name__ == "__main__":
 
     #Importaciones
     Funcionesgenerales = FuncionesGenerales()
-    Funciones = Modulos()
 
     
-
-    
+    ################
     Funcionesgenerales.mensajebienvenida()
-    
+    #[ Abrir archivo Data]#
     datatexto1 = abrirarchivodata()
-
     print(datatexto1)
+
+    
 
 
     
