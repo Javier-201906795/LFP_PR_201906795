09455ab6630d4cf9995f7eb3c2162bd7e24c72d3
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2022-02-19 17:25:47 -0600

    listar variables de configuracion
    lista los datos proprocionas por .lfp

--- 1_Proyecto/FuncionesConfig.py
+++ 1_Proyecto/FuncionesConfig.py
@@ -11,10 +11,76 @@ class FuncionesConfig:
         self.TituloY = "N/A"
 
     def clasificador(self):
+        #Texto Original
         dataconfig = self.dataconfig
+        #Texto Minusculas
         dataconfigmin = dataconfig.lower()
-        print(dataconfig)
-        print(dataconfigmin)
+        #Texto sin Espacios y en minusculas
+        dataconfigsinespacios = ""
+        for a in dataconfigmin:
+                if a == '\n' or a == ' ':
+                    pass
+                else:
+                    dataconfigsinespacios += a
+        #print(dataconfig)
+        #print(dataconfigmin)
+        #print(dataconfigsinespacios)
+
+
+        #Quitar "¿"
+        cont = 0
+        for c in dataconfigmin:
+                cont += 1
+                if c == "¿":
+                    break
+        #Temp Texto
+        tempdataconfimin = dataconfigmin[cont:len(dataconfigmin)]
+        print(tempdataconfimin,"\n---------------------------\n")
+
+        #Quitar "?>"
+        cont = 0
+        for d in tempdataconfimin:
+                cont += 1
+                if d == ">":
+                    break
+        #Temp Texto
+        tempdataconfimin = tempdataconfimin[0:cont - 2]
+        print(tempdataconfimin,"\n||||||||||||||||||||\n")
+
+        #Quitar "\n"
+        cont = 0
+        temptext = ""
+        for e in tempdataconfimin:
+            cont += 1
+            if e == "\n":
+                pass
+            else:
+                temptext += e
+        
+
+        #Separar ","
+        listtemptext = temptext.split(",")
+        print(listtemptext)
+
+        #Buscar nombre
+        variable = ""
+        for item in listtemptext:
+            print(item)
+            #encontrar ":"
+            for c in item:
+                if c == ":":
+                    break
+            #Extraer "nombre:"
+            
+            
+            
+
+        
+
+
+
+        
+        
         return None
 
     ################################################################
