d1b0031b2a6a0a4622445d00ee66489cec465cc2
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2022-02-19 20:10:58 -0600

    test.js
    agrega items al html

--- 1_Proyecto/FuncionesReporte.py
+++ 1_Proyecto/FuncionesReporte.py
@@ -27,5 +27,60 @@ mytexto.innerHTML = 'Hola COMO ESTAS' """
     def crearReporte(self):
 
         #Crear JAVASCRIPT
-        self.javascriptdatos("dato1")
+        try:
+            #Abre el archivo
+            f = open('LFP_PR_201906795.js','w')
+            #[Nuevo contenido]
+            #Id del documento
+            contenido = """// Producto TOP
+topnombre = document.getElementById("topnombre");
+topcantidad = document.getElementById("topcantidad");
+topprecio = document.getElementById("topprecio");
+topvendido = document.getElementById("topvendido");
+// Producto Menos Vendido
+bottomnombre = document.getElementById("bottomnombre");
+bottomcantidad = document.getElementById("bottomcantidad");
+bottomprecio = document.getElementById("bottomprecio");
+bottomvendido = document.getElementById("bottomvendido");
+//Tabla 
+tablacuerpo = document.getElementById("tablacuerpo");"""
+            #Producto Mas Vendido
+            contenido += """// Producto TOP
+
+topnombre.innerHTML = 'Tortrix' 
+topcantidad.innerHTML = 5 
+topprecio.innerHTML = 25.5 
+topvendido.innerHTML = 50.05 """        
+            #Producto Menos Vendido
+            contenido += """// Producto Menos Vendido
+
+bottomnombre.innerHTML = 'Chocolate' 
+bottomcantidad.innerHTML = 1 
+bottomprecio.innerHTML = 0.5 
+bottomvendido.innerHTML = 0.5 """
+            #Tabla datos
+            contenido += """//Tabla
+var listaproductos = [{nombre:"Paleta",cantidad: 25,precio:5.50},{nombre:"Paleta2",cantidad: 215,precio:52.50},{nombre:"Paleta3",cantidad: 5,precio:1.50}]"""
+            #Tabla Imprimir
+            contenido += """console.log(listaproductos)
+
+//Limpiar Tabla
+tablacuerpo.innerHTML = ''
+
+for (var i = 0; i < listaproductos.length; i++){
+    tablacuerpo.innerHTML += '<tr>'+
+    '<th scope="row">'+(i+1)+'</th>' +
+    '<td>'+listaproductos[i].nombre+'</td>' +
+    '<td>Q '+listaproductos[i].precio+'</td>' +
+    '<td>'+listaproductos[i].cantidad+'</td>' +
+    '</tr>';
+}"""
+
+            f.write(contenido)
+            f.close()
+        except:
+            print("Error al modicar archivo javascript")
+
+
+
         return None
--- 1_Proyecto/test.js
+++ 1_Proyecto/test.js
@@ -28,12 +28,9 @@ bottomprecio.innerHTML = 0.5
 bottomvendido.innerHTML = 0.5 
 
 //Tabla
-
 var listaproductos = [{nombre:"Paleta",cantidad: 25,precio:5.50},{nombre:"Paleta2",cantidad: 215,precio:52.50},{nombre:"Paleta3",cantidad: 5,precio:1.50}]
 
 console.log(listaproductos)
-console.log(listaproductos[0])
-console.log(listaproductos[0].nombre)
 
 //Limpiar Tabla
 tablacuerpo.innerHTML = ''
